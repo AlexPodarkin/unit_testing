58cea88f8417123207114555e6518c4c8b1a0cc7
AleksandrPodarkin <alexchembarov@gmail.com>
2023-11-30 17:07:33 +0400

    hw3

--- home_work_3/main_unittest.py
+++ home_work_3/main_unittest.py
@@ -4,9 +4,16 @@ from home_work_3.main import Tasks
 
 class Test(unittest.TestCase):
     def test_even_odd_number_test(self):
-        self.assertTrue(Tasks.even_odd_number(10))
-        self.assertFalse(Tasks.even_odd_number(11))
+        self.assertTrue(Tasks.even_odd_number(4))
+
+    def test_even_not_odd_number_test(self):
+        self.assertFalse(Tasks.even_odd_number(5))
 
     def test_interval(self):
-        self.assertTrue(Tasks.number_in_interval(30))
-        self.assertFalse(Tasks.number_in_interval(10))
+        self.assertTrue(Tasks.number_in_interval(50))
+
+    def test_not_interval(self):
+        self.assertFalse(Tasks.number_in_interval(25))
+
+    def test_not_interval_up(self):
+        self.assertFalse(Tasks.number_in_interval(100))
